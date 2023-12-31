b24ccba1d572d600b35066288197d301baa0b70a
Auza2008 <auza2008@outlook.com>
2023-12-31 13:02:40 +0800

    startclient and stopclient

--- src/main/dev/hermes/Client.java
+++ src/main/dev/hermes/Client.java
@@ -1,10 +1,19 @@
 package dev.hermes;
 
 import lombok.Getter;
+import org.lwjgl.opengl.Display;
 
 @Getter
 public class Client {
-    private final String name = "Hermes";
-    private final String version = "dev 0.1";
-    private final String title = name + " " + version + " | " + "Minecraft 1.8.9";
+    private static final String name = "Hermes";
+    private static final String version = "dev 0.1";
+    private static final String title = name + " " + version + " | " + "Minecraft 1.8.9";
+
+    public static void startClient() {
+        Display.setTitle(title);
+    }
+
+    public static void stopClient() {
+
+    }
 }
