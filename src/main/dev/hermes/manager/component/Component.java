31d474a2719ea0814a828be138487c72d6359b89
neilhuang007 <neilhuang007@gmail.com>
2024-01-12 04:31:21 -0800

    full rewrite of range and stuff moved class packages, if you want just move em back
    needa fix the crash on init

--- src/main/dev/hermes/manager/component/Component.java
+++
@@ -1,7 +0,0 @@
-package dev.hermes.manager.component;
-
-import net.minecraft.client.Minecraft;
-
-public abstract class Component {
-    public static Minecraft mc = Minecraft.getMinecraft();
-}
--- src/main/dev/hermes/value/Function0.java
+++
@@ -1,6 +0,0 @@
-package dev.hermes.value;
-
-public interface Function0<R> {
-    R invoke();
-}
-
